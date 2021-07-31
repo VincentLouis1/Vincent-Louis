d3a042613a38f488d4168f53c0303b3f1cfd6b79
VincentLouis1 <87592957+VincentLouis1@users.noreply.github.com>
2021-07-31 19:06:01 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,10 @@
 int main()
 {
   FILE *fp;
-  char filename[13],line[200],s1[]="int",s2[]="void",
-                                  s3[]="float",s4[]="double",s5[]=";",s6[]="(",s7[]=")",s9[]="return",
-                                  s10[]="printf" ,s11[]="fprintf" ,s12[]="fputs",s14[]="if",s15[]="while";
-
-char s13[]="}";
-int x;
-  printf("enter filename:\n");
-  gets(filename);
+  char filename[]="test.c",line[200],s1[]="int",s2[]="void",
+  s3[]="float",s4[]="double",s5[]=";",s6[]="(",s7[]=")" ,s9[]="return" ,s10[]="printf" ,s11[]="fprintf" ,s12[]="fputs";
+char s13[]="}",s14[] =" ";
+  int x=0,y=0;
   fp=fopen(filename,"r");
   char str[200];
   if(!fp)
@@ -20,26 +16,34 @@ int x;
   exit(0);
 }
 while ( fgets ( line, 200 , fp ) != NULL ) 
-{
-  if((strstr(line,s1) || strstr(line,s2) || strstr(line,s3) || strstr(line,s4)) && !strstr(line,s14)
-                      && !strstr(line,s15) && !strstr(line,s5) && strstr(line,s6) && strstr(line,s7))
+{ 
+  if((strstr(line,s1) || strstr(line,s2) || strstr(line,s3) || strstr(line,s4)) && !strstr(line,s5) && strstr(line,s6) && strstr(line,s7))
    {
       x=1;
       strcpy(str,line);
    }
   if(strstr(line,s9) || strstr(line,s10) || strstr(line,s11) || strstr(line,s12))
     {
-     x++;
+     y++;
     }
     if(strstr(line,s13))
-{
-x=x+2;
-}
-if(x==3)
+   { 
+    x++;
+   }
+   if(line[0] == '\n')
+   {
+       y++;
+   }
+if(x==2&&y==0)
 {
 fputs(str,stdout);
 x=0;
+y=0;
 }
+if(strstr(line,s13))
+   { 
+    y=0;
+   }
 }
   fclose ( fp );
   return 0;
